644802e8e42d35788226fdc8c2e2ba9677267c0a
SANTHOSHKUMAR T <96092429+santhoshkumartm@users.noreply.github.com>
2026-09-03 16:11:19 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=len(nums)
+        # count=0
+        # for num in nums:
+        #     count=sum(1 for i in nums if i==num)
+        #     if count>n//2:
+        #         return num
+
+        freq={}
+        max_count=0
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+            max_count=max(max_count,freq[num])
+            if max_count > n//2:
+                return num
